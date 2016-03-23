82269f870f67e6f04693eb30af918bffbe4a6395
Shivam sharma <shivams334@gmail.com>
2016-03-24 00:03:19 +0530

    Update skrollr.py

--- skrollr.py
+++ skrollr.py
@@ -1,7 +1,7 @@
 from Tkinter import *
 
 root = Tk()
-scrollbar = Scrollbar(ro0t)
+scrollbar = Scrollbar(root)
 scrollbar.pack( side = RIGHT, fill=Y )
 
 mylist = Listbox(root, yscrollcommand = scrollbar.set )
