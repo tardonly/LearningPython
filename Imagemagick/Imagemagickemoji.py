46e4ac3d0782732ed8d3d22eee8430dd333a782d
Shivam sharma <shivams334@gmail.com>
2016-10-23 13:30:06 +0530

    Update Imagemagickemoji.py

--- Imagemagick/Imagemagickemoji.py
+++ Imagemagick/Imagemagickemoji.py
@@ -4,14 +4,14 @@ from wand.image import Image
 from wand.display import display
 
 
-fg_url = 'http://i.stack.imgur.com/Mz9y0.jpg'
-bg_url = 'http://i.stack.imgur.com/TAcBA.jpg'
+fg_url = 'http://bit.ly/2ek2g6j'
+bg_url = 'http://bit.ly/2ewFjd5'
 
 bg = urllib2.urlopen(bg_url)
 with Image(file=bg) as bg_img:
     fg = urllib2.urlopen(fg_url)
     with Image(file=fg) as fg_img:
-        bg_img.composite(fg_img, left=400, top=250)
+        bg_img.composite(fg_img, left=320, top=240)
     fg.close()
     display(bg_img)
 bg.close()
